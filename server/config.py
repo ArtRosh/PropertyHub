27deaab19199610e9e81940a8e69520fc7112fde
Artem Roshchupkin <artem.rosh17@gmail.com>
2026-04-10 14:49:04 -0700

    Modified version of config in order to hold security key in .env file

--- server/config.py
+++ server/config.py
@@ -1,31 +1,35 @@
-from key import secret_key
+import os
 from flask import Flask
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
 from flask_bcrypt import Bcrypt
+from sqlalchemy import MetaData
+from dotenv import load_dotenv
 
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///app.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
-app.secret_key = secret_key
+
+
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev-secret')
+
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
-db = SQLAlchemy(metadata=metadata)
 
-migrate = Migrate(app, db)
+
+db = SQLAlchemy(metadata=metadata)
 db.init_app(app)
 
+migrate = Migrate(app, db)
 api = Api(app)
-
 CORS(app)
-
 bcrypt = Bcrypt(app)
-
-key = secret_key
