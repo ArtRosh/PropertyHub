6a739ecb5063d08bb597f3bef56c6a62d27c9596
Artem Roshchupkin <artem.rosh17@gmail.com>
2026-04-10 08:53:16 -0700

    Add authentication and role-based user model

--- server/models.py
+++ server/models.py
@@ -1,5 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from config import db
+from sqlalchemy.ext.hybrid import hybrid_property
+from config import bcrypt
 
 
 class Property(db.Model):
@@ -19,8 +21,21 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True)
+    _password_hash = db.Column(db.String)
+    role = db.Column(db.String, default="visitor")
+
     property_users = db.relationship("PropertyUser", back_populates="user", cascade="all, delete-orphan")
 
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError("Password is not readable")
+
+    @password_hash.setter
+    def password_hash(self, password):
+        self._password_hash = bcrypt.generate_password_hash(password).decode("utf-8")
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password)
 
 class Review(db.Model):
     __tablename__ = "reviews"
