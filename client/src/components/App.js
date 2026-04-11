d30652e4c2e37ca3ad8bddd20e16ab45fbfa80f5
Artem Roshchupkin <artem.rosh17@gmail.com>
2026-04-11 12:15:54 -0700

    Add data fetchning

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 
 function App() {
   const [properties, setProperties] = useState([]);
   const [reviews, setReviews] = useState([]);
   const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    fetch("/properties")
+      .then((res) => res.json())
+      .then(setProperties);
+  }, []);
+
+  useEffect(() => {
+    fetch("/reviews")
+      .then((res) => res.json())
+      .then(setReviews);
+  }, []);
+
+  useEffect(() => {
+    fetch("/images")
+      .then((res) => res.json())
+      .then(setImages);
+  }, []);
 
   return (
     <Switch>
