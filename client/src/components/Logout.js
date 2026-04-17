30507b6cc52daf6db1dc1ddce757795ffe335e37
Artem Roshchupkin <artem.rosh17@gmail.com>
2026-04-17 12:16:54 -0700

    Add logout logic

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -1,3 +1,18 @@
-export default function Logout() {
-  return <div>Logout</div>;
+import React from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+function Logout({setLogout}){
+    const history = useHistory()
+    function handleLogout(){
+        fetch("/logout", {
+            method: "DELETE",
+        }).then(() => setLogout())
+        history.push('/')
+    }
+    
+    return(
+        <header>
+            <button onClick={handleLogout} type="button">Logout</button>
+        </header>
+    )
 }
+export default Logout
